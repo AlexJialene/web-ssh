f79aaceb268d3e5d7ecf815acf3e62ce64ffad03
linjiayu <467146659@qq.com>
2020-03-24 18:28:46 +0800

    js

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
+	"golang.org/x/crypto/ssh"
 	"html/template"
 	"net/http"
 )
@@ -21,27 +22,34 @@ var (
 )
 
 func wsHandle(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
+	var (
+		conn    *websocket.Conn
+		client  *ssh.Client
+		sshConn *SSHConnect
+		err     error
+	)
+	if conn, err = upgrader.Upgrade(w, r, nil); err != nil {
 		return
 	}
 	defer conn.Close()
 
 	//Create ssh client
-	if client, err := createSSHClient(user, password, host, port); err == nil {
-		defer client.Close()
-		//connect to ssh
-		sshConn, err := NewSSHConnect(client)
-		if err != nil {
-			fmt.Println(err)
-		}
+	if client, err = createSSHClient(user, password, host, port); err != nil {
+		WsSendText(conn, []byte(err.Error()))
+		return
+	}
+	defer client.Close()
 
-		quit := make(chan int)
-		go sshConn.Output(conn, quit)
-		go sshConn.Recv(conn, quit)
-		<-quit
+	//connect to ssh
+	if sshConn, err = NewSSHConnect(client); err != nil {
+		WsSendText(conn, []byte(err.Error()))
+		return
 	}
 
+	quit := make(chan int)
+	go sshConn.Output(conn, quit)
+	go sshConn.Recv(conn, quit)
+	<-quit
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
